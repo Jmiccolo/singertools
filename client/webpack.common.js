b568f7cc99e743fce6668acb52980285d0d635d2
Josh Miccolo <jmiccolo89@gmail.com>
2020-12-01 12:35:13 -0500

    Add Dependecies and Install

---
+++ client/webpack.common.js
@@ -0,0 +1,12 @@
+const path = require("path");
+module.exports = {
+    entry:"./src/index.js",
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use:["style-loader", "css-loader", "sass-loader"]
+            }   
+        ]
+    }
+}
